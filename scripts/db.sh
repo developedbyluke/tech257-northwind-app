afa1f6db9fcb3f57265026c3b557b8ca49156920
developedbyluke <lukehorton7@yahoo.com>
2024-04-03 12:16:28 +0100

    Update scripts

--- scripts/db.sh
+++ scripts/db.sh
@@ -2,20 +2,21 @@
 
 sudo apt update && sudo apt upgrade -y
 
-sudo apt install mysql-server -y
+if ! dpkg -s mysql-server >/dev/null 2>&1; then
+    sudo apt-get install mysql-server -y
+fi
 
 sudo mysql <<EOF
 CREATE DATABASE northwind;
 CREATE USER 'admin'@'%' IDENTIFIED BY 'password';
 GRANT ALL PRIVILEGES ON northwind.* TO 'admin'@'%';
 FLUSH PRIVILEGES;
-EXIT;
 EOF
 
 cd ../app
 sudo mysql northwind < northwind_sql.sql
 
 # Comment out bind-address in mysql config
-sudo sed -i 's/bind-address/#bind-address/g' /etc/mysql/mysql.conf.d/mysqld.cnf
+sudo sed -i 's/bind-address.*/bind-address = 0.0.0.0/g' /etc/mysql/mysql.conf.d/mysqld.cnf
 
 sudo systemctl restart mysql
